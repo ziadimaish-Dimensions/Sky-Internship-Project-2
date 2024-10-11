a72f1169a380255474bc80c8dbad6891876bae89
Ziad Omaish <ziadomaish@dimensions-studio.com>
2024-10-11 13:38:59 +0300

    adding employee service

---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from '../models/employee.model';
+import { VacationRequest } from '../models/vacation-request.model';
+import { VacationHistory } from '../models/vacation-history.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeService {
+  private apiUrl = 'assets/data'; // Assuming JSON files are in assets/data
+
+  constructor(private http: HttpClient) {}
+
+  getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiUrl}/employees.json`);
+  }
+
+  getVacationRequests(): Observable<VacationRequest[]> {
+    return this.http.get<VacationRequest[]>(`${this.apiUrl}/vacation-requests.json`);
+  }
+
+  getVacationHistory(): Observable<VacationHistory[]> {
+    return this.http.get<VacationHistory[]>(`${this.apiUrl}/vacation-history.json`);
+  }
+
+  // Add methods for adding, updating, deleting data as necessary
+}
