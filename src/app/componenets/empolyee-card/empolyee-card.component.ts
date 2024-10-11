42dce5e2e8f44f72e83887a80132aa3893c74302
Ziad Omaish <ziadomaish@dimensions-studio.com>
2024-10-11 13:34:20 +0300

    creating components

---
+++ src/app/componenets/empolyee-card/empolyee-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empolyee-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './empolyee-card.component.html',
+  styleUrl: './empolyee-card.component.css'
+})
+export class EmpolyeeCardComponent {
+
+}
